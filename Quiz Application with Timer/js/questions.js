f48dfbb3981a188995bb802fc149938bef3abd5e
Vardhan098 <98535589+Vardhan098@users.noreply.github.com>
2022-01-27 21:16:37 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,113 @@
+
+let questions = [
+  {
+    numb: 1,
+    question: "India has signed agreements with which country for Visa Facilitation and Leather Technology??",
+    answer: "Ethiopia",
+    options: [
+      "USA",
+      "Iran",
+      "Ethiopia",
+      "Japan"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which state has passed an ordinance giving co-ownership rights to women in husband’s ancestral property?",
+    answer: "Uttarakhand",
+    options: [
+      "Tamil Nadut",
+      "Kerala",
+      "Uttarakhand",
+      "West Bengal"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Dr Syama Prasad Mookerjee Institute of Medical Sciences and Research’ has been set up by which institution?",
+    answer: "IIT Kharagpur",
+    options: [
+      "IIT Madras",
+      "IIT Delhi",
+      "IIT Kharagpur",
+      "IIT Kanpur"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which organisation has prepared a draft national migrant labour policy in India?",
+    answer: "NITI Aayoge",
+    options: [
+      "NITI Aayoge",
+      "Reserve Bank of India",
+      "FICCI",
+      " ASSOCHAM"
+    ]
+  },
+  {
+    numb: 5,
+    question: "As per the new Information Technology Rules, digital platforms will have to provide required information within how many hours?",
+    answer: "72 hours",
+    options: [
+      "12 hours",
+      "24 hours",
+      "48 hours",
+      "72 hours"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which organisation is to commission India’s largest floating solar power plant at Telangana?",
+    answer: "NHPC",
+    options: [
+      "NHPC",
+      "NTPC",
+      "REC",
+      "BHEL"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which organisation has extended its support for India’s H2Ooooh! – Waterwise Program?",
+    answer: "UNESCO",
+    options: [
+      "World Bank",
+      "UNESCO",
+      "UNICEF",
+      "OECD"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which organisation has proposed to develop world class ‘Wayside Amenities’ at more than 600 locations in India?",
+    answer: "NHAI",
+    options: [
+      " NTPC",
+      "BHEL",
+      "NHAI",
+      "BRO"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Anandam: The Center for Happiness” has been inaugurated at which institution?",
+    answer: "IIM Jammu",
+    options: [
+      "IIT Bombay",
+      "IIT Madras",
+      " IIM Jammu",
+      "IIT Guwahati"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is “IH2A” that has been seen in the news recently?",
+    answer: "India H2 Alliance",
+    options: [
+      " Covid 19 vaccine candidate",
+      "Meteor",
+      "New Plant species",
+      "India H2 Alliance"
+    ]
+  },
+];
